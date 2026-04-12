6998ca26801b701bd50f75b65d35d442bd525e91
ElXus123 <64488753+ElXus123@users.noreply.github.com>
2026-04-12 21:33:51 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado:", socket.id);
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("voice-signal", (data) => {
+    socket.to(data.room).emit("voice-signal", data);
+  });
+
+  socket.on("location-update", (data) => {
+    socket.to(data.room).emit("location-update", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
